d3901c7f47ada18cb8f1ed66c98bb78b1f4c9744
ahmed_douyry <ahmed.douyry@gmail.com>
2024-01-16 23:46:45 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ListeEtudiants } from './Listeetudiants.jsx';
+import { ModifierEtudiant } from './ModifierEtudiant';
+import  { AjouterEtudiant } from './ajouterEtudiant';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route index path='/' element={<ListeEtudiants />}/>
+      <Route  path='/modifier/:id' element={<ModifierEtudiant />}/>
+      <Route  path='/ajouterEtudiant/:laid' element={<AjouterEtudiant />}/>
+    </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
