c5a11ad145610ff3dd445b3df978ae00278b8ff3
ahmed_douyry <ahmed.douyry@gmail.com>
2024-01-17 01:23:01 +0100

    last-one

--- src/Reducer.jsx
+++ src/Reducer.jsx
@@ -1,8 +1,8 @@
 const initialState = [
     {id:1,nom:'douyry',prenom:'ahmed' ,email:'ahmed.douyry@gmail.com',sexe:'masculin'},
     
-    {id:3,nom:'douyry',prenom:'youssef' ,email:'youssef.douyry@gmail.com',sexe:'masculin'},
-    {id:2,nom:'sarouti',prenom:'Najwa' ,email:'Najwa_sarouti@gmail.com',sexe:'feminin'},
+    {id:2,nom:'douyry',prenom:'youssef' ,email:'youssef.douyry@gmail.com',sexe:'masculin'},
+    {id:3,nom:'sarouti',prenom:'Najwa' ,email:'Najwa_sarouti@gmail.com',sexe:'feminin'},
 ]
 
 
